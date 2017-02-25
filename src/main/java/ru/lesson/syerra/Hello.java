5dcbf7f318af65e89581b4f5d3f4a84e2b04ad93
IrinaFF <irina.fatkullina@gmail.com>
2017-02-25 19:43:50 +0500

    структура мавен

---
+++ src/main/java/ru/lesson/syerra/Hello.java
@@ -0,0 +1,14 @@
+package ru.lesson.syerra;
+
+/**
+ * TODO: comment
+ *
+ * @autor irinaff
+ * @since 25.02.2017
+ **/
+
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello!");
+    }
+}
